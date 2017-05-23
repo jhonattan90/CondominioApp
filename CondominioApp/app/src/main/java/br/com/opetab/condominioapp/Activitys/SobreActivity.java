35db88edc8db8bc9cd45d554597bc555f4328aba
jhonattan90 <charles.lima@atrativaweb.com.br>
2017-05-22 21:07:59 -0300

    menu lateral, tela sobre , tracar rota

--- CondominioApp/app/src/main/java/br/com/opetab/condominioapp/Activitys/SobreActivity.java
+++ CondominioApp/app/src/main/java/br/com/opetab/condominioapp/Activitys/SobreActivity.java
@@ -1,8 +1,14 @@
 package br.com.opetab.condominioapp.Activitys;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
 
 import br.com.opetab.condominioapp.R;
 
@@ -13,6 +19,17 @@ public class SobreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
         setupToolbar();
+
+        Button btnRota = (Button) findViewById(R.id.btnRota);
+        btnRota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uri = "http://maps.google.com/maps?daddr=" + "-25.391150" + "," + "-49.297024" + " (" + "Chamacon" + ")";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                intent.setPackage("com.google.android.apps.maps");
+                startActivity(intent);
+            }
+        });
     }
 
     public void setupToolbar(){
